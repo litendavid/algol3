9fbe5d02971cfea0e1340d65e30b90d85730ee02
KungD <david@krawaller.se>
2014-08-05 19:25:44 +0200

    initial commit

---
+++ algol.js
@@ -0,0 +1,13 @@
+(function(){
+
+var Algol = {};
+
+
+
+// €€€€€€€€€€€€€ EXPORTS
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    module.exports = Algol;
+  else
+    window.Algol = Algol;
+})();
---
+++ test/specs/algolspec.js
@@ -0,0 +1,8 @@
+
+var Algol = (typeof require !== undefined ? require("../algol.js") : window.Algol);
+
+describe("Algol",function(){
+	it("should be defined",function(){
+		expect(Algol).toBeDefined();
+	});
+});
