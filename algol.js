6e614db1d60739cb69cd5e63c5baa5624259b919
KungD <david@krawaller.se>
2014-08-05 20:05:48 +0200

    added calcpropval func and tests

--- algol.js
+++ algol.js
@@ -2,6 +2,19 @@
 
 var Algol = {};
 
+/**
+	 * Calculates property state at a given time according to startvalue and changes
+	 * @param {Number|String} startvalue
+	 * @param {Array} changes List of changes, each change is [step,value]
+	 * @param {Number} step
+	 * @return {Number|String} Value at given time
+ */
+Algol.calcPropVal = function(startvalue,changes,step){
+	if (!changes || !changes.length ){ return startvalue; } // no change, return startval
+	if ((!step) || step > changes[changes.length-1][0]){ return changes[changes.length-1][1]; } // no time given or time after last change, return last change value
+	if (step < changes[0][0]){ return startvalue; } // time prior to first change, return start val
+	return _.find(changes.slice().reverse(),function(change){ return change[0] <= step; })[1]; // last change val before step
+};
 
 
 // €€€€€€€€€€€€€ EXPORTS
--- test/specs/algolspec.js
+++ test/specs/algolspec.js
@@ -1,8 +1,16 @@
 
-var Algol = (typeof require !== undefined ? require("../../algol.js") : window.Algol);
+var Algol = (typeof require !== undefined ? require("../../algol.js") : window.Algol),
+	_ = (typeof require !== undefined ? require("lodash") : window._),
+	R = _.uniqueId;
 
-describe("Algol",function(){
-	it("should be defined",function(){
-		expect(Algol).toBeDefined();
+describe("The time functions",function(){
+	describe("The calcPropVal function",function(){
+		var cpv = Algol.calcPropVal;
+		describe("When called with undefined changes",function(){
+			var startval = R(), result = cpv(startval);
+			it("should return the startval",function(){
+				expect(result).toEqual(startval);
+			});
+		});
 	});
 });
